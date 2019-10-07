9df3d1447c9869c3117100c0204dffe2acd92aea
GTech1256 <pomah156@gmail.com>
2019-10-07 13:21:59 +0300

    добавляет REST API и инцилизацию окна ошибки

---
+++ js/backend.js
@@ -0,0 +1,78 @@
+'use strict';
+
+(function () {
+
+  /* CONSTANTS */
+
+  var TIMEOUT = 10000; // 10s
+  var RESPONSE_TYPE = 'json';
+  var API_LINK = {
+    picture: 'https://js.dump.academy/kekstagram/data',
+  };
+  var METHOD = {
+    get: 'GET',
+    post: 'POST'
+  };
+
+  /**
+   * Создает инстанс XHR с обработчиками и делает запрос
+   *
+   * @param {function(*): void} onLoad
+   * @param {function(string): void} onError
+   * @param {string} url путь HTTP
+   * @param {('GET'|'POST')} method Метод HTTP
+   * @param {*} [data] данные HTTP
+   */
+  function makeXHR(onLoad, onError, url, method, data) {
+    var xhr = new XMLHttpRequest();
+
+    XMLHttpRequest.responseType = RESPONSE_TYPE;
+    xhr.timeout = TIMEOUT; // 10s
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(JSON.parse(xhr.response));
+        return;
+      }
+
+      onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.open(method, url);
+    xhr.send(data);
+  }
+
+  /* FUNCTIONS */
+
+  /**
+   * @param {function(*): void} onLoad
+   * @param {function(string): void} onError
+   */
+  function loadPictures(onLoad, onError) {
+    makeXHR(onLoad, onError, API_LINK.picture, METHOD.get);
+  }
+
+  /**
+   * @param {FormData} data
+   * @param {function(*): void} onLoad
+   * @param {function(string): void} onError
+   */
+  // function save(data, onLoad, onError) {
+  //   makeXHR(onLoad, onError, API_LINK.formSetupSend, METHOD.post, data);
+  // }
+
+  /* EXPORT */
+
+  window.backend = {
+    loadPictures: loadPictures,
+    // save: save
+  };
+})();
--- js/picture-render.js
+++ js/picture-render.js
@@ -4,17 +4,17 @@
 
   /* CONSTANTS */
 
-  var MIN_COUNT_LIKES = 15;
-  var MAX_COUNT_LIKES = 200;
-  var COMMENTS = [
-    'Всё отлично!', 'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ];
-  var DESCRIPTION = 'описание фотографии.';
-  var NAMES = ['Артем', 'Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  // var MIN_COUNT_LIKES = 15;
+  // var MAX_COUNT_LIKES = 200;
+  // var COMMENTS = [
+  //   'Всё отлично!', 'В целом всё неплохо. Но не всё.',
+  //   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  //   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  //   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  //   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  // ];
+  // var DESCRIPTION = 'описание фотографии.';
+  // var NAMES = ['Артем', 'Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 
 
   /* VARIABLES */
@@ -23,34 +23,6 @@
 
 
   /* FUNCTIONS */
-
-  /**
-   * @param {number} count
-   * @return {Picture[]}
-   */
-  function getGeneratedPictures(count) {
-    var pictures = [];
-
-    for (var i = 0; i <= count - 1; i++) {
-      pictures.push({
-        url: 'photos/' + (i + 1) + '.jpg',
-        description: DESCRIPTION,
-        likes: window.utils.getRandomIntInclusive(MIN_COUNT_LIKES, MAX_COUNT_LIKES),
-        comments: [{
-          avatar: 'img/avatar-' + window.utils.getRandomIntInclusive(1, 6) + '.svg',
-          message: window.utils.getRandomValueFromArray(COMMENTS),
-          name: window.utils.getRandomValueFromArray(NAMES),
-        },
-        {
-          avatar: 'img/avatar-' + window.utils.getRandomIntInclusive(1, 6) + '.svg',
-          message: window.utils.getRandomValueFromArray(COMMENTS),
-          name: window.utils.getRandomValueFromArray(NAMES),
-        }]
-      });
-    }
-
-    return pictures;
-  }
 
   /**
    * создает DOM-элемент,
@@ -92,7 +64,6 @@
   /* EXPORT */
 
   window.pictureRender = {
-    getGeneratedPictures: getGeneratedPictures,
     renderGeneratedPictures: renderGeneratedPictures
   };
 
